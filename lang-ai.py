372672783f33e4ee4c4ffa68d36878be7c2c37e1
ishitaagarwal <ish.agarwal.1@gmail.com>
2024-11-12 21:24:11 +0000

    Adding a prompt to get information summary

--- lang-ai.py
+++ lang-ai.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
+import os
+
+from dotenv import load_dotenv
+import os
+
+if __name__ == "__main__":
+    load_dotenv()
     print("Hello Lang AI!")
+    print(os.environ["COOL_API_KEY"])
